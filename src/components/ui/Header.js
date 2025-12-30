1ecb1204bbafe1f4cb39cd3d33081c3a6f25872e
HungPig <hungthanhhung37@gmail.com>
2025-12-30 20:53:38 +0700

    Add OCI image index, manifest, and layout for react-todo
    - Created index.json to define the OCI image index with the latest tag.
    - Added manifest.json to specify the configuration and layers for the react-todo image.
    - Introduced oci-layout file to indicate the image layout version.

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -5,6 +5,7 @@ export default function Header(props) {
     return (
         <header>
             <h1>Things To Do</h1>
+            <h2>Dương Thanh Hưng</h2>
             <InputWrapper {...props}/>
         </header>
     );
